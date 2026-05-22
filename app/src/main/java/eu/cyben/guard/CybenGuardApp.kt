dc1d7ed94d548cafd8b7df8070ff77d4667306d2
rubio105 <rg.consulenza@gmail.com>
2026-05-22 22:19:08 +0200

    feat: integra OneSignal SDK per notifiche push Android

--- app/src/main/java/eu/cyben/guard/CybenGuardApp.kt
+++ app/src/main/java/eu/cyben/guard/CybenGuardApp.kt
@@ -1,7 +1,21 @@
 package eu.cyben.guard
 
 import android.app.Application
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
-class CybenGuardApp : Application()
+class CybenGuardApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        OneSignal.Debug.logLevel = LogLevel.NONE
+        OneSignal.initWithContext(this, "31270916-7dbe-4d64-817f-cbb7aa808060")
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(false)
+        }
+    }
+}
