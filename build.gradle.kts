84e4cdad507404ee4d3fce3994ee4319f6e41247
rubio105 <rg.consulenza@gmail.com>
2026-05-23 13:46:08 +0200

    fix: aggiorna Hilt a 2.52 per supporto Kotlin 2.1.0 metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
