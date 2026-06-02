e211db2fa867ad4f84a0a46d72a96ee5db199f96
rubio105 <rg.consulenza@gmail.com>
2026-06-02 19:41:47 +0200

    fix: aggiorna Kotlin 2.2.20, AGP 8.7.3, Gradle 8.11.1 per compatibilità dipendenze

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.dagger.hilt.android") version "2.56" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    kotlin("kapt") version "2.0.21" apply false
+    kotlin("kapt") version "2.2.20" apply false
 }
